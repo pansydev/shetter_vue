a5bcb12738ecb3bfa437ae44877f874431e7df9f
Exeteres <exeteres@yandex.ru>
2021-03-23 21:54:16 +0700

    fix: resolve backend connection issue and update models

--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,24 @@ import graphql from "@rollup/plugin-graphql";
 export default ({ mode }: { mode: string }) => {
   process.env = {
     ...process.env,
-    ...loadEnv(mode, process.cwd())
+    ...loadEnv(mode, process.cwd()),
   };
 
   return defineConfig({
     plugins: [vue(), graphql()],
     resolve: {
       alias: {
-        "@shetter": resolve(__dirname, "src")
-      }
+        "@shetter": resolve(__dirname, "src"),
+      },
     },
     server: {
       proxy: {
         "/graphql": {
-          target: process.env.VITE_API_URL ?? "http://localhost:5000/graphql"
-        }
-      }
-    }
+          target: process.env.VITE_API_URL ?? "http://localhost:5000/graphql",
+          changeOrigin: true,
+          secure: false
+        },
+      },
+    },
   });
-}
+};
