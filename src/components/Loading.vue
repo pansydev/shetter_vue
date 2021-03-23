2db6bac52a38556ee670045bc482f52db3dbf923
alexthvest <alexthvest@gmail.com>
2021-03-23 20:18:30 +0300

    feat: add infinity scroll of post list

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,16 +1,3 @@
 <template>
-  <span v-if="visible" class="material-icons-outlined animate-spin">motion_photos_on</span>
+  <span class="material-icons-outlined animate-spin">motion_photos_on</span>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  props: {
-    visible: {
-      type: Boolean,
-      default: false,
-    },
-  },
-});
-</script>
