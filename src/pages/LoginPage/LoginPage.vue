9c37a2b69ec788d661ec0d38079c6848ee8a6258
alexthvest <alexthvest@gmail.com>
2021-03-24 19:47:38 +0300

    feat: add login page

---
+++ src/pages/LoginPage/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <ShetterContainer class="space-y-6 w-full 2xl:w-3/12 lg:w-5/12 md:w-7/12">
+    <header class="flex flex-col items-center space-y-2">
+      <h1 class="text-xl font-semibold">Авторизация в Shetter</h1>
+      <p class="text-sm text-gray-500">Введите данные, чтобы войти в аккаунт</p>
+    </header>
+    <main class="space-y-2">
+      <input type="text" placeholder="Логин" v-model="username" />
+      <input type="password" placeholder="Пароль" v-model="password" />
+    </main>
+    <footer class="flex space-x-2">
+      <button class="flex-1">Войти</button>
+      <a class="button dark flex-1" href="register.html">Регистрация</a>
+    </footer>
+  </ShetterContainer>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+import ShetterContainer from "@shetter/components/ShetterContainer.vue";
+
+export default defineComponent({
+  components: {
+    ShetterContainer,
+  },
+  setup() {
+    const username = ref<string>();
+    const password = ref<string>();
+
+    return { username, password };
+  },
+});
+</script>
--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,11 +1,16 @@
 ﻿import { createRouter, createWebHashHistory } from "vue-router";
 
 import MainPage from "@shetter/pages/MainPage/MainPage.vue";
+import LoginPage from "@shetter/pages/LoginPage/LoginPage.vue";
 
 const routes = [
   {
     path: "/",
     component: MainPage,
+  },
+  {
+    path: "/login",
+    component: LoginPage,
   },
 ];
 
