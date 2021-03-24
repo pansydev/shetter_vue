8c68d7d586372801b9a4e9d35e8cb712f0151880
alexthvest <alexthvest@gmail.com>
2021-03-24 18:23:35 +0300

    refactor: move post form to component

---
+++ src/pages/MainPage/components/PostForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <form method="post" class="flex items-center pr-4 border border-gray-200 rounded-md h-auto">
+    <textarea
+      class="rounded-md border-none resize-none overflow-hidden text-sm w-full mr-auto p-4 focus:ring-0"
+      placeholder="Начните писать текст..."
+    ></textarea>
+    <button class="dark py-3" type="submit">Опубликовать</button>
+  </form>
+</template>
