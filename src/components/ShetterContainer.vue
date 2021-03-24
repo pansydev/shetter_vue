15f861ed92949a05ff9d3a2a91d15b57d376748a
alexthvest <alexthvest@gmail.com>
2021-03-24 19:47:17 +0300

    refactor: main container moved to common components

---
+++ src/components/ShetterContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-8 shadow rounded-md bg-white" style="height: fit-content">
+    <slot></slot>
+  </div>
+</template>
--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,25 +1,27 @@
 ﻿<template>
-  <main class="space-y-6">
+  <ShetterContainer class="space-y-6 md:my-4 w-full 2xl:w-4/12 lg:w-6/12 md:w-8/12">
     <header class="flex items-center">
-      <div class="title mr-auto">
+      <div class="mr-auto">
         <h1 class="text-lg font-semibold">Shetter</h1>
-        <span class="text-sm text-gray-500">@unknown</span>
+        <p class="text-sm text-gray-500">@unknown</p>
       </div>
-      <a class="button" href="/login">Войти в аккаунт</a>
+      <RouterLink class="button" to="/login">Войти в аккаунт</RouterLink>
     </header>
     <PostForm />
     <PostList />
-  </main>
+  </ShetterContainer>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 import PostList from "@shetter/pages/MainPage/components/PostList.vue";
-import PostForm from "@shetter/pages/MainPage/components/PostForm";
+import PostForm from "@shetter/pages/MainPage/components/PostForm.vue";
+import ShetterContainer from "@shetter/components/ShetterContainer.vue";
 
 export default defineComponent({
   components: {
+    ShetterContainer,
     PostForm,
     PostList,
   },
