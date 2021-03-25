97a06e0b8faa37089a8db7f86aa82c81780be6d0
alexthvest <alexthvest@gmail.com>
2021-03-25 20:57:39 +0300

    refactor: add defineComponent to component, remove unnecessary button class

--- src/pages/MainPage/components/AppHeader.vue
+++ src/pages/MainPage/components/AppHeader.vue
@@ -5,16 +5,15 @@
       <p v-if="authenticated" class="text-sm text-gray-500">@{{ username }}</p>
     </div>
     <RouterLink v-if="!authenticated" class="button" to="/login">Войти в аккаунт</RouterLink>
-    <button v-else @click="logout" class="button">Выйти</button>
+    <button v-else @click="logout">Выйти из аккаунта</button>
   </header>
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { defineComponent, computed } from "vue";
 import { tokenManager } from "@shetter/utils/tokenManager";
 
-export default {
-  name: "AppHeader",
+export default defineComponent({
   props: {
     authenticated: {
       type: Boolean,
@@ -32,5 +31,5 @@ export default {
 
     return { username, logout };
   },
-};
+});
 </script>
